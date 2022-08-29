a5ff456bad61d888e0d51b13b4b26f839f080ca0
Mateusz Kasprzak <kacpak@gmail.com>
2022-08-29 10:54:38 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+# kvp-cli
+
+Key - Value Pair CLI
+
+## Usage
+
+```
+Usage: kvp-cli [options] [command]
+
+CLI to edit key-value pair files
+
+Options:
+  -V, --version                    output the version number
+  -h, --help                       display help for command
+
+Commands:
+  get <config-file> <key>          returns a value for a key
+  set <config-file> <key> <value>  sets key-value pair
+  unset <config-file> <key>        removes key from file
+  help [command]                   display help for command
+```
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import ini from 'ini';
+import { program } from 'commander';
+import { get, set, unset } from 'lodash-es';
+
+const resolveFilePath = (value: string) => path.resolve(value);
+const resolveFilePathOrThrow = (value: string) => {
+  const filePath = resolveFilePath(value);
+  if (!fs.existsSync(filePath)) {
+    program.error(`File "${filePath}" does not exist!`, {
+      code: 'ERR001',
+      exitCode: 2,
+    });
+  }
+  return filePath;
+};
+
+program.name('kvp-cli').description('CLI to edit key-value pair files').version('1.0.0').allowExcessArguments(false);
+
+program
+  .command('get')
+  .description('returns a value for a key')
+  .argument('<config-file>', 'path to configuration file to read', resolveFilePathOrThrow)
+  .argument('<key>', 'key to read')
+  .action((configFilePath, key) => {
+    const config = ini.parse(fs.readFileSync(configFilePath, 'utf-8'));
+    console.log(get(config, key) ?? '');
+  });
+
+program
+  .command('set')
+  .description('sets key-value pair')
+  .argument('<config-file>', 'path to configuration file to edit', resolveFilePath)
+  .argument('<key>', 'key to set')
+  .argument('<value>', 'new value for a key')
+  .action((configFilePath, key, value) => {
+    const configExists = fs.existsSync(configFilePath);
+    const config = configExists ? ini.parse(fs.readFileSync(configFilePath, 'utf-8')) : {};
+    set(config, key, value);
+    const newConfigContent = ini.stringify(config);
+    if (!configExists) {
+      fs.mkdirSync(path.dirname(configFilePath));
+    }
+    fs.writeFileSync(configFilePath, newConfigContent);
+  });
+
+program
+  .command('unset')
+  .description('removes key from file')
+  .argument('<config-file>', 'path to configuration file to edit', resolveFilePathOrThrow)
+  .argument('<key>', 'key to unset')
+  .action((configFilePath, key) => {
+    const config = ini.parse(fs.readFileSync(configFilePath, 'utf-8'));
+    unset(config, key);
+    const newConfigContent = ini.stringify(config);
+    fs.writeFileSync(configFilePath, newConfigContent);
+  });
+
+program.parse();
